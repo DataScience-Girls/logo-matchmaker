c08d9489ff7375996a1065c5ea45bb3aed359500
Konstantin Perikov <pericov.ko@gmail.com>
2023-04-03 22:34:00 +0100

    feat: update to use efficient net

--- resnet.py
+++ resnet.py
@@ -1,11 +1,17 @@
+import os
+import pickle
+
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from tensorflow.keras.applications.efficientnet_v2 import (
+    EfficientNetV2L,
+    preprocess_input,
+)
 from tensorflow.keras.preprocessing import image
 
 
 def load_and_preprocess_image(img_path, target_size=(224, 224)):
-    img = image.load_img(img_path, target_size=target_size)
+    img = image.load_img(img_path)
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = preprocess_input(img_array)
@@ -18,7 +24,7 @@ def extract_features(img_path, model):
     return features
 
 
-def find_similar_logos(logo_path, database, model, threshold=0.7):
+def find_similar_logos(logo_path, database, model, threshold=0.9):
     logo_features = extract_features(logo_path, model)
     similar_logos = []
 
@@ -30,29 +36,54 @@ def find_similar_logos(logo_path, database, model, threshold=0.7):
     return sorted(similar_logos, key=lambda x: x[1], reverse=True)
 
 
-# Load pre-trained ResNet50 model (without the top layer)
-resnet50_model = ResNet50(weights="imagenet", include_top=False, pooling="avg")
+efficientnet_model = EfficientNetV2L(
+    weights="imagenet", include_top=False, pooling="avg"
+)
 
-# Assume we have a dictionary called "logo_database" containing the paths of
-# logo images as keys and their features as values
-logo_database = {
-    "data/logo1.jpg": extract_features("data/logo1.jpg", resnet50_model),
-    "data/logo2.jpg": extract_features("data/logo2.jpg", resnet50_model),
-    "data/logo3.jpg": extract_features("data/logo3.jpg", resnet50_model),
-    "data/logo4.jpg": extract_features("data/logo4.jpg", resnet50_model),
-    "data/logo5.jpg": extract_features("data/logo5.jpg", resnet50_model),
-    "data/logo6.jpg": extract_features("data/logo6.jpg", resnet50_model),
-    "data/logo7.jpg": extract_features("data/logo7.jpg", resnet50_model),
-    "data/logo8.jpg": extract_features("data/logo8.jpg", resnet50_model),
-    "data/logo9.jpg": extract_features("data/logo9.jpg", resnet50_model),
-    "data/logo10.jpg": extract_features("data/logo10.jpg", resnet50_model),
-    # ...
-}
+logodatabase_file_path = "data/logo_database.pkl"
+data_folder = "data/images"
 
+# Check if the file exists
+if os.path.exists(logodatabase_file_path):
+    print(f"The file '{logodatabase_file_path}' exists.")
+else:
+    print(f"The file '{logodatabase_file_path}' does not exist.")
+    logo_database = {}
+    for file_name in os.listdir(data_folder):
+        file_path = os.path.join(data_folder, file_name)
+
+        # Check if the file is an image (assuming only JPEG and PNG formats)
+        if file_path.lower().endswith((".jpg", ".jpeg", ".png")):
+            logo_database[file_path] = extract_features(file_path, efficientnet_model)
+
+    with open(logodatabase_file_path, "wb") as file:
+        pickle.dump(logo_database, file)
+    print("Logo database saved to logo_database.pkl")
+
+
+loaded_logo_database = {}
+with open(logodatabase_file_path, "rb") as file:
+    loaded_logo_database = pickle.load(file)
+
+print("Logo database loaded from logo_database.pkl")
+
+all_files = os.listdir(data_folder)
+
+# Filter out only image files (assuming only JPEG and PNG formats)
+image_files = [
+    file for file in all_files if file.lower().endswith((".jpg", ".jpeg", ".png"))
+]
+
+# Select a random image file
+# target_logo_file = random.choice(image_files)
+# target_logo_path = os.path.join(data_folder, target_logo_file)
+target_logo_path = "target1.jpg"
 # Find similar logos to the target logo
-target_logo_path = "data/target_logo.jpg"
-similar_logos = find_similar_logos(target_logo_path, logo_database, resnet50_model)
+similar_logos = find_similar_logos(
+    target_logo_path, loaded_logo_database, efficientnet_model
+)
 
 # Print the similar logos and their similarity scores
+print(f"Finding similar logo to {target_logo_path}")
 for logo_path, similarity in similar_logos:
     print(f"Logo: {logo_path}, Similarity: {similarity[0][0]}")
